db3b9ba2dbfbc5d58df8c37c2ef0f4555cbb0054
Anton Nekipelov <anton.nekipelov@gmail.com>
2014-11-28 11:58:19 +0100

    add grunt release plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,37 @@
 /* jshint node: true */
 'use strict';
-module.exports = function(grunt) {
-    grunt.initConfig({
-        watch: {
-            less: {
-                files: ['src/**/*.less'],
-                tasks: ['less:material']
-            }
-        },
+module.exports = function (grunt) {
+    grunt.initConfig(
+        {
 
-        less: {
-          material: {
-            files: {
-              'src/material.css': 'src/material.less'
-            }            
-          }
+            release: {
+                options: {
+                    file: 'bower.json',
+                    npm: false
+                }
+            },
+
+            watch: {
+                less: {
+                    files: ['src/**/*.less'],
+                    tasks: ['less:material']
+                }
+            },
+
+            less: {
+                material: {
+                    files: {
+                        'src/material.css': 'src/material.less'
+                    }
+                }
+            }
         }
-    });
+    );
 
     //load tasks
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-release');
 
     //build task
     grunt.registerTask('build', [
