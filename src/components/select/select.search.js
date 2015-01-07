68bd413d2fb809ecab0b748585af1d2785bc401a
Tommy Maintz <tmaintz@schubergphilis.com>
2015-01-07 10:47:51 +0100

    Already in an apply

--- src/components/select/select.search.js
+++ src/components/select/select.search.js
@@ -85,7 +85,7 @@ materialComponents.directive('materialSelectsearch', [
 
                     input.on('keypress', function(e) {
                         if (e.keyCode === Constants.KEY_CODE.ENTER) {
-                            $scope.$apply(function() {
+                            $scope.$evalAsync(function() {
                                 if (!hasRenderedValue && $scope.results.length) {
                                     var value = $scope.results[0][$scope._valueField];
                                     menus.select($scope.selectId, value);
